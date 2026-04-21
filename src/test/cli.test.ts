56632d2cc56afa3ac256dd7d48267951bda50b46
Joshua Bracks <joshabracks@gmail.com>
2026-04-21 10:54:49 -0700

    remove vestigal test

--- src/test/cli.test.ts
+++ src/test/cli.test.ts
@@ -6,9 +6,7 @@
 import { runEngineFixtureTests } from './engine-fixtures.test.js';
 import { reportAndExit } from './harness.js';
 import { runOmosceneTests } from './omoscene.test.js';
-import { runSmokeTests } from './smoke.test.js';
 
-runSmokeTests();
 runOmosceneTests();
 await runEngineFixtureTests();
 reportAndExit();
